84af2d4ed335f77388cfeba4cbed29a2d5318378
Nazanin Farshad <nazaninf24@gmail.coms>
2019-07-11 17:09:30 +0200

    First commit

---
+++ 03_stairway.rb
@@ -0,0 +1,16 @@
+
+def average_finish_time
+
+marche = 0
+puts rand(1..6)
+if (rand = 5 || rand = 6)
+        marche = marche + 1   
+        puts "Tu es à la marche #{marche}"
+    elsif (rand = 2 || rand = 3 || rand = 4)
+        puts "rien ne se passe, tu es toujours à la marche #{marche}"
+    else rand = 1
+       marche = marche - 1
+        puts " Tu es descendu à la marche #{marche}"
+end
+
+average_finish_time
